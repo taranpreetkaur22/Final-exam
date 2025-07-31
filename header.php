c33eb3ea1ab02312afc79837bee681d1de4f4336
taranpreetkaur22 <taranpreetkaur01@algomau.ca>
2025-07-31 11:09:57 -0400

    test

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
 <html lang="en">
 <head>
     <title>Final Exam</title>
-    <link rel="icon" href="https://algomau.ca/wp-content/uploads/2017/07/favicon.ico">
+    <link rel="icon" href="https://algomau.ca/wp-content/uploads/2020/02/favicon-32x32-1.png">
 </head>
 <body>
